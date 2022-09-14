1ffed32220a80325bbbf40ceacaa5f0be632225a
Dennis Ngari <ngaridennis33@gmail.com>
2022-09-14 14:14:16 -0700

    Added a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "putchar.h"
+
+/**
+ * main - Prints _putchar
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints _putchar w/out stdio
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0. program that prints _putchar, followed by a new line
