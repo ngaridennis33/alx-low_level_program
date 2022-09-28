fb420c382733ffc30ba113257ea6980a3b915ece
Dennis Ngari <ngaridennis33@gmail.com>
2022-09-28 10:26:43 -0700

    Added a function that prints a string, followed by a new line

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int factorial(int n);
+int _strlen_recusion(char *s);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt(int n, int x);
+int is_prime_numbers(int n);
+int is_prime(int n, int start);
+void check(char *s, int start, int end, int *flag);
+
+#endif /*MAIN_H*/
