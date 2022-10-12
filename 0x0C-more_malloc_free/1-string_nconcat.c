2257830182edba1e2dcd6b04dce770b96c482f43
Dennis Ngari <ngaridennis33@gmail.com>
2022-10-12 02:42:24 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,12 +20,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		for (i = 0; s1[i]; i++)
+			;
 	}
 	if (s2 == NULL)
 		j = 0;
 	else
 	{
 		for (j = 0; s2[j]; j++)
+			;
 	}
 	if (j > n)
 		j = n;
