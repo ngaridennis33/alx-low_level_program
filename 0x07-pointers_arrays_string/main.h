1e0018d20622f41f83bfd727abf5e7d8845261f7
Dennis Ngari <ngaridennis33@gmail.com>
2022-10-16 01:30:47 -0700

    0-memset.c

--- 0x07-pointers_arrays_string/main.h
+++ 0x07-pointers_arrays_string/main.h
@@ -1,12 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
-* void prototypes()
-* int prototypes()
-*/
-
-int _putchar(char);
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -16,5 +8,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif /*MAIN_H*/
